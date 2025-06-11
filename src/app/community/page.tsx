05efc1486324f1d216a641efbe076a98d9a18015
sathvika-ramagiri <ramagirisathvika31@gmail.com>
2025-06-11 14:50:19 +0530

    Fix: Export default component for community page

---
+++ src/app/community/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CommunityPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Community Page</h1>
+      <p>This page is under construction!</p>
+    </div>
+  );
+};
+
+export default CommunityPage;
